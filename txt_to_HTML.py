fcd8aeebc6344a235f3c62128da1254f10b0603a
Marco <marco.pasqua03@gmail.com>
2023-09-11 21:33:46 -0400

    Updated formatting and added comments

--- txt_to_HTML.py
+++ txt_to_HTML.py
@@ -18,101 +18,103 @@ def remove_output_dir(output_dir): # function to remove output directory if it e
 
 def text_to_html(input_path, stylesheet, output_dir):
     try:
-
-
-        html_header = f"<html lang='en'>\n<head>\n\t<meta charset='utf-8'>\n"  # Writing the first part of the html file
-
         if os.path.exists(input_path) and os.path.isdir(input_path):  # if the user inputted a directory then convert all the txt files to html:
-            remove_output_dir(output_dir)
+            remove_output_dir(output_dir) # removing and/or creating the output_dir
 
-            for filename in os.listdir(input_path):
-                if filename.endswith(".txt"):  #
-                    output_file = os.path.splitext(filename)[0] + ".html"
-                    input_file = os.path.join(input_path, filename)
-                    output_file = os.path.join(output_dir, output_file)
+            for filename in os.listdir(input_path): # for loop to convert each txt file to html
+                if filename.endswith(".txt"):  # making sure that if any other files exist in the folder that it only grabs the ones that end with .txt
+                    output_file = os.path.splitext(filename)[0] + ".html" # creating the name of the output_file to have the name of the file currently being processed and end with .html
+                    input_file = os.path.join(input_path, filename) # getting the current input file name and its path so it can be combined
+                    output_file = os.path.join(output_dir, output_file) # getting the path of the output directory and combining it with name of the output_file
 
                     # opening the input file
-                    with open(input_file, "r") as txt:
-                        lines = txt.readlines()
+                    with open(input_file, "r") as txt: # opening the input file for reading. Using the with keyword automatically closes it once it's done
+                        lines = txt.readlines() # reading all the lines from the current file
 
 
-                    html_contents = html_header
-                    title = os.path.splitext(os.path.basename(input_file))[
+                    html_contents = f"<html lang='en'>\n<head>\n\t<meta charset='utf-8'>\n" # creating variable that will hold everything needed to create the HTML file and writing the first part of the file
+
+                    title = os.path.splitext(os.path.basename(input_file))[ # Getting the name of the file and removing the path name and txt extension so it can be used as the title for the html tag
                         0]  # Creating the title based off of the filename
                     html_contents += f"""\n \t<title>{title}</title>\n\t<meta name='viewport' content='width=device-width, initial-scale=1'>{f'<link rel="stylesheet" type="text/css" href="{stylesheet}">'}
                     \n</head>\n<body>\n"""
 
-                    for line in lines:
+                    for line in lines: # for loop to read through all the lines in the file and make changes if required
                         if line == "\n":  # If there is a new line in the txt file, add the <br> tag
                             html_contents += f"<br>\n"
-                        else:  # Otherwise add the <p> tag to the line
-                            converted_line = line.replace("\n",
-                                                          "")  # Removing the newline terminator at the line of the line to help make the html formatting clearer to read
+                        else:  # if not remove the newline terminator at the end of the line and add the <p> tag to the current line
+                            converted_line = line.replace("\n","")
                             html_contents += f"<p>{converted_line}</p>\n"
 
                     html_contents += f"</body>\n</html>"  # Adding in the closing part of the HTML
 
-                    with open(output_file, "w") as html:
+                    with open(output_file, "w") as html: # opening the output html file for writing
                         html.write(html_contents)
 
-                print("File conversion was successful! Please look for the ", output_dir ," folder.")
+                print("File conversion was successful! Please look for the ", output_dir ," folder.") # printing success message
 
         elif input_path.endswith(".txt") and os.path.isfile(input_path):
-            remove_output_dir(output_dir)
+            remove_output_dir(output_dir) # removing and/or creating the output_dir
 
-            output_file = os.path.splitext(input_path)[0] + ".html"
-            input_file = input_path
-            output_file = os.path.join(output_dir, output_file)
-
-            # Opening the input file for reading
-            with open(input_file, "r") as txt:
-                lines = txt.readlines()
+            output_file = os.path.splitext(input_path)[0] + ".html" # creating the name of the output_file to have the name of the file currently being processed and end with .html
+            input_file = input_path # Getting the input_file from the input_path
+            output_file = os.path.join(output_dir, output_file) # getting the path of the output directory and combining it with name of the output_file
 
 
-            html_contents = html_header
+            with open(input_file, "r") as txt: # Opening the input file for reading
+                lines = txt.readlines() # reading all the lines from the input_file
+
+
+            html_contents = f"<html lang='en'>\n<head>\n\t<meta charset='utf-8'>\n" # creating variable that will hold everything needed to create the HTML file and writing the first part of the file
             title = os.path.splitext(os.path.basename(input_file))[0]  # Creating the title based off of the filename
             html_contents += f"""\n \t<title>{title}</title>\n\t<meta name='viewport' content='width=device-width, initial-scale=1'>{f'<link rel="stylesheet" type="text/css" href="{stylesheet}">'}
                                 \n</head>\n<body>\n"""
 
-            for line in lines:
-                if line == "\n":
+            for line in lines: # for loop to read through all the lines in the file and make changes if required
+                if line == "\n": # if there is a new line add in the <br> tag
                     html_contents += f"<br>"
-                else:
+                else: # if not remove the newline terminator at the end of the line and add the <p> tag to the current line
                     converted_line = line.replace("\n", "")
                     html_contents += f"<p>{converted_line}</p>\n"
 
             html_contents += f"</body>\n</html>"
 
-            with open(output_file, "w") as html:
+            with open(output_file, "w") as html: # opening the output html file for writing
                 html.write(html_contents)
-            print("File conversion was successful! Please look for the ", output_dir, " folder.")
-        else:
+            print("File conversion was successful! Please look for the ", output_dir, " folder.") # printing success message
+
+        else: # if the file/folder entered does not exist print this message
             print("The file/directory name does not exist. Please make sure you entered the correct name")
 
-    except Exception as e:
+    except Exception as e: # Throw an error if any kind of error happens
         print(f"An error occurred: {str(e)}")
 
 
 if __name__ == '__main__':
     # NOTE: Description allows for a description of the program to show up when -h or --help is entered
     # NOTE: epilog allows for us to provide the user with an example of how to call on/run the program
+    # Line of code to help assist the user with a brief description on how to use the program
     parser = argparse.ArgumentParser(description="Convert a text file to an HTML file.",
                                      epilog="Example: python main.py input.txt or python main.py ./folder")
-    parser.add_argument("input_path",
-                        help="Path to the input text file or directory")  # The help keyword on this line and the line below  will allow the program to show the user the argument that is needed to run the program
 
     # Line of code below will allow a --version or -v to work
     parser.add_argument("--version", "-v", action="version", version=f"%(prog)s {VERSION}")
 
-    #optional feature 1
+    # Creating a variable to take in the input_path argument from the user
+    parser.add_argument("input_path",
+                        help="Path to the input text file or directory")  # The help keyword on this line and the line below  will allow the program to show the user the argument that is needed to run the program
+
+    # Optional argument to use the stylesheet feature
     parser.add_argument("--stylesheet", "-s", help="Use if you want to add a stylesheet to the generated HTML file")
 
-    #optional feature 2
+    # Optional argument to use the output directory feature
     parser.add_argument("--output", "-o", help="Use if you want to change the destined output of the HTML files. Otherwise it will be sent to the til folder")
 
-    args = parser.parse_args()
+    args = parser.parse_args() # parsing the arguments from the command line
+
+    # creating variables to take in command line arguments
     input_path = args.input_path
     stylesheet = args.stylesheet
-    output_dir = args.output or "./til"
+    output_dir = args.output or "./til" # If the user does not enter an output directory, it will assign the directory til
 
     text_to_html(input_path, stylesheet, output_dir)
