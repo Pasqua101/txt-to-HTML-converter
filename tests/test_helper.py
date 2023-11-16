42db55452bccf21e4054b2701020eda485f07880
Marco <marco.pasqua03@gmail.com>
2023-11-15 22:18:36 -0500

    Created a tester for sidebar function and changed the logic of checking the file type of the sidebar

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,6 +1,8 @@
+import os
+import tempfile
 import unittest
 
-from helper import extension_checker, output_file_creator
+from helper import extension_checker, output_file_creator, generate_sidebar
 
 
 class TestExtensionChecker(unittest.TestCase):
@@ -26,6 +28,43 @@ class TestOutputFileCreatorFileName(unittest.TestCase):
     def test_unable_to_create_file(self):
         self.assertRaises(Exception, output_file_creator, 1, "test")
         self.assertRaises(Exception, output_file_creator, "test.md", 1)
+
+
+class TestGenerateSidebar(unittest.TestCase):
+    def setUp(self):
+        self.table_of_contents = [
+            {
+                "label": "TIL How to Get Command Line Arguments in Python",
+                "url": "TIL How to Get Command Line Arguments in Python.html",
+            },
+            {
+                "label": "TIL rmtree and os Library",
+                "url": "TIL rmtree and os Library.html",
+            },
+        ]
+
+        self.sidebar_file = "sidebar.py"
+
+        self.invalid_file = "invalid.md"
+
+    def test_generate_sidebar(self):
+        result = generate_sidebar(self.sidebar_file)
+
+        expected = (
+            "\t\tTable of Contents\n\t\t"
+            "<nav>\n\t\t\t"
+            "<ul>\n\t\t\t\t<li>"
+            '<a href="TIL How to Get Command Line Arguments in Python.html">TIL How to Get Command Line Arguments in Python</a>'
+            "</li>\n\t\t\t\t"
+            '<li><a href="TIL rmtree and os Library.html">TIL rmtree and os Library</a>'
+            "</li>\n\t\t\t</ul>\n\t\t</nav>"
+        )
+
+        self.assertEqual(result, expected)
+
+    def test_generate_sidebar_error(self):
+        with self.assertRaises(SystemExit):
+            generate_sidebar(self.invalid_file)
 
 
 if __name__ == "__main__":
